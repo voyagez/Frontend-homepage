f8b98633f87ad208a32a05d20e9baa8d8b27be16
avbbarker <avbbarker@icloud.com>
2023-08-29 17:28:55 -0400

    Spelling

--- frontend-part-ii/client/src/components/footer/Footer.jsx
+++ frontend-part-ii/client/src/components/footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
           <li className="fListItem">Terms & conditions</li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2023 Voyagez Booking</div>
+      <div className="fText">Copyright © 2023 Voyage Booking</div>
     </div>
   );
 };
